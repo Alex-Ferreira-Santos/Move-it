f1dc034d1a61b3984d55e5059ee5d1064dd25d70
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-02-24 11:25:27 -0300

    challenges

--- moveit-next/src/components/ExperienceBar.tsx
+++ moveit-next/src/components/ExperienceBar.tsx
@@ -1,13 +1,17 @@
+import {useContext} from 'react'
+import { ChallengesContext } from '../context/ChallengesContext';
 import styles from '../styles/components/ExperienceBar.module.css';
+
 export function ExperienceBar(){
+    const {currentExperience,experienceToNextLevel} = useContext(ChallengesContext)
     return(
         <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
                 <div style={{width: '50%'}}/>
-                <span className={styles.currentExperience} style={{left:'50%'}}>300xp</span>
+                <span className={styles.currentExperience} style={{left:'50%'}}>{currentExperience}xp</span>
             </div>
-            <span>600 xp</span>
+            <span>{experienceToNextLevel} xp</span>
         </header>
     )
 }
