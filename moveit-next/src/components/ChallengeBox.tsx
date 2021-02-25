4e6ec20976d046def92af64713c14df6f10fe972
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-02-25 10:16:55 -0300

    reinicio do timer

--- moveit-next/src/components/ChallengeBox.tsx
+++ moveit-next/src/components/ChallengeBox.tsx
@@ -1,10 +1,22 @@
 import { useContext } from 'react'
 import { ChallengesContext } from '../context/ChallengesContext'
+import { CountdownContext } from '../context/CountdownContext';
 import styles from '../styles/components/ChallengeBox.module.css'
 
 export function ChallengeBox(){
 
     const {activeChallenge,resetChallenge, completeChallenge} = useContext(ChallengesContext);
+    const { resetCountDown} = useContext(CountdownContext)
+
+    function handleChallengeSucceeded(){
+        completeChallenge();
+        resetCountDown()
+    }
+
+    function handleChallengeFailed(){
+        resetChallenge()
+        resetCountDown()
+    }
 
     return(
         <div className={styles.challengeBoxContainer}>
@@ -20,12 +32,12 @@ export function ChallengeBox(){
                         <button
                             type="button"
                             className={styles.challengeFailedButton}
-                            onClick={resetChallenge}
+                            onClick={handleChallengeFailed}
                         >Falhei</button>
                         <button
                             type="button"
                             className={styles.challengeSucceededButton}
-                            onClick={completeChallenge}
+                            onClick={handleChallengeSucceeded}
                         >Completei</button>
                     </footer>
                 </div>
