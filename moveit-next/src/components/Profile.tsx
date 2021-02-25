aa39cefce45881ec742d6f57b69b88d8ba3b3709
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-02-25 09:53:07 -0300

    funcionalidade de ganho de xp

--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../context/ChallengesContext'
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile(){
+    const { level } = useContext(ChallengesContext)
     return(
         <div className={styles.profileContainer}>
             <img src="https://github.com/Alex-Ferreira-Santos.png" alt="Alex Ferreira Santos"/>
@@ -8,7 +11,7 @@ export function Profile(){
                 <strong>Alex Ferreira Santos</strong>
                 <p>
                     <img src="icons/level.svg" alt="level"/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
